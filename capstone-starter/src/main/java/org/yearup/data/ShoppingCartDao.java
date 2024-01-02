dd067593b1909d828c9416005d0979d96e93ed65
Ren Das <aaryan.das.42gmail.com>
2024-01-02 13:11:51 -0600

    completed CategoryDao and started ShoppingCartDao

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,18 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    List<Product> getShoppingCartContents (int userId);
+    ShoppingCart create(ShoppingCart shoppingCart);
+    void update(int userId, int productId, int quantity);
+    void clear(int userId);
+
 }
