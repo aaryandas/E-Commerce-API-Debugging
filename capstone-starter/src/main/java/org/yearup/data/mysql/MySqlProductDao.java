1e1caf4ca0fbbb63a7f75d7f4cded96058256e2f
Supriya Roberson <supriyaroberson@Supriyas-MBP.home>
2024-01-02 14:44:59 -0600

    updated mysqlproductdao

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -21,7 +21,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color){
         List<Product> products = new ArrayList<>();
 
-        String sql = "SELECT * FROM products WHERE (category_id = ? OR ? = -1) AND (price <= ? OR ? = -1) AND (color = ? OR ? = '') ";
+        String sql = "SELECT * FROM products WHERE (category_id = ? OR ? = -1) AND (price BETWEEN ? AND ? OR price = -1) AND (color = ? OR ? = '')";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
